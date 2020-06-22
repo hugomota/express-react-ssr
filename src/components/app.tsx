1963bbe73eb78d56ac6663821335fc3b77c0b962
Hugo Mota <hugo.mota@checkmarx.com>
2020-06-22 14:07:10 +0100

    first commit

---
+++ src/components/app.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react'
+
+export interface HelloProps {}
+
+const App = () => <h1>Hello from!</h1>
+
+export default App
---
+++ src/routes/api.ts
@@ -0,0 +1,9 @@
+import express from 'express'
+
+const apiRoutes = express.Router()
+
+apiRoutes.get('/', async (_req, res) => {
+
+})
+
+export default apiRoutes
